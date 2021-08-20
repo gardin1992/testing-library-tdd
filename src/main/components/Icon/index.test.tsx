f33bad173c018f3d307414ab37d101efaf867f18
João Vieira <joao.vieira@marttech.com.br>
2021-08-20 10:56:42 -0300

    added test click

--- src/main/components/Icon/index.test.tsx
+++ src/main/components/Icon/index.test.tsx
@@ -1,12 +1,23 @@
-import { screen, render } from "@testing-library/react";
+import { screen, render, fireEvent } from "@testing-library/react";
 import { ReactComponent as MenuSvg } from "assets/icons/menu-closed.svg";
 import Icon from ".";
 
 it("should render icon from svg", () => {
-  render(<Icon icon={MenuSvg} />);
+  let clicked = false;
 
-  const icon = screen.getByRole("button").closest("i");
-  const svg = screen.getByRole("img");
+  function onClick(event: React.MouseEvent<HTMLElement>) {
+    event.preventDefault();
+    clicked = true;
+  }
+
+  render(<Icon icon={MenuSvg} onClick={onClick} />);
+
+  const icon = screen.getByRole("button");
   expect(icon).toBeInTheDocument();
+
+  const svg = screen.getByRole("img");
   expect(svg).toBeInTheDocument();
+
+  fireEvent.click(icon);
+  expect(clicked).toBe(true);
 });

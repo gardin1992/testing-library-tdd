1fe2fe04ca18a92bde66868a926f12bfad013505
João Vieira <joao.vieira@marttech.com.br>
2021-08-20 10:41:31 -0300

    reset styles

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,12 @@
 import { StrictMode } from "react";
 import ReactDOM from "react-dom";
 import Router from "main/Router";
+import GlobalStyle from "styles";
+import "@csstools/normalize.css";
 
 ReactDOM.render(
   <StrictMode>
+    <GlobalStyle />
     <Router />
   </StrictMode>,
   document.getElementById("root")

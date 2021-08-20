d35927a6d574cd90aaea1e65c9c1114e3299498b
João Vieira <joao.vieira@marttech.com.br>
2021-08-20 11:04:36 -0300

    open function outise click

--- src/main/hooks/useOutsideClick.tsx
+++ src/main/hooks/useOutsideClick.tsx
@@ -2,14 +2,9 @@ import { useEffect } from "react";
 
 export function useOutsideClick(
   ref: React.RefObject<HTMLElement>,
-  callback: () => void
+  handleClickOutside: any
 ) {
   useEffect(() => {
-    function handleClickOutside(event: any) {
-      if (ref?.current !== null && !ref?.current.contains(event.target)) {
-        callback();
-      }
-    }
     document.addEventListener("click", handleClickOutside);
     return () => {
       document.removeEventListener("click", handleClickOutside);

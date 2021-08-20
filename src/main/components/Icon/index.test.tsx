4e27cdd04a449c4a84615f25b0f566d635145a86
João Vieira <joao.vieira@marttech.com.br>
2021-08-20 10:42:21 -0300

    alter icon add onClick

--- src/main/components/Icon/index.test.tsx
+++ src/main/components/Icon/index.test.tsx
@@ -1,10 +1,12 @@
 import { screen, render } from "@testing-library/react";
-import { ReactComponent as MenuSvg } from "assets/icons/menu-close.svg";
+import { ReactComponent as MenuSvg } from "assets/icons/menu-closed.svg";
 import Icon from ".";
 
 it("should render icon from svg", () => {
   render(<Icon icon={MenuSvg} />);
 
-  const svg = screen.getByRole("button").closest("i");
+  const icon = screen.getByRole("button").closest("i");
+  const svg = screen.getByRole("img");
+  expect(icon).toBeInTheDocument();
   expect(svg).toBeInTheDocument();
 });
--- src/main/components/Icon/index.tsx
+++ src/main/components/Icon/index.tsx
@@ -1,23 +1,37 @@
+import * as S from "./styles";
+
 type Props = {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   icon: any;
-  size?: number;
   color?: string;
+  variant?: string;
+  size?: number;
+  title?: string;
+  onClick?: (event: React.MouseEvent<HTMLElement>) => void;
 };
 
 function Icon(props: Props) {
-  const { icon: Component, size, color } = props;
+  const { icon: Component, size, variant, color, title, onClick } = props;
 
   return (
-    <i className="c-icon" role="button">
-      <Component width={size} height={size} fill={color} />
-    </i>
+    <S.Icon className={`c-icon ${variant}`} role="button" onClick={onClick}>
+      <Component
+        width={size}
+        height={size}
+        fill={color}
+        role="img"
+        title={title}
+      />
+    </S.Icon>
   );
 }
 
 Icon.defaultProps = {
   size: 24,
+  variant: "",
   color: "#000000",
+  title: "icon",
+  onClick: undefined,
 };
 
 export default Icon;
